64a88789fa65f5be8e66b8d8dba71188f5e55855
rushabh <rushabh@sellergize.com>
2024-02-09 17:52:29 +0530

    jason data showed on html page

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
+async function getUsers() {
+  try {
 
-const publickey = '9f39169afffeef58dbd798b9c0171075';
-const privatekey = '71ee64bfae3a3e53f87eedbdac50145bcb1c416a';
-const currentDate = new Date().getTime();
-const hashvalue = CryptoJS.MD5(currentDate+privatekey+publickey).toString();
+  const publickey = '9f39169afffeef58dbd798b9c0171075';
+  const privatekey = '71ee64bfae3a3e53f87eedbdac50145bcb1c416a';
+  const currentDate = new Date().getTime();
+  const hashvalue = CryptoJS.MD5(currentDate+privatekey+publickey).toString();
 
-const apiUrl = `http://gateway.marvel.com/v1/public/comics?ts=${currentDate}&apikey=${publickey}&hash=${hashvalue}`;
+  const apiUrl = `http://gateway.marvel.com/v1/public/characters?ts=${currentDate}&apikey=${publickey}&hash=${hashvalue}`;
 
-const options = { 
-    method: 'GET', 
-    headers: { 
-        Accept: '*/*', 
-    },  
-  };
+  const options = { 
+      method: 'GET', 
+      headers: { 
+          Accept: '*/*', 
+      },  
+    };
 
-fetch(apiUrl, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  }).then(response =>{
-    console.log(response);
-  }).catch((error) => {
-    console.log(error)
-  });
+  const data = await fetch(apiUrl, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    });
+    // .then(response =>{
+    //   console.log(response);
+    // });
+
+    return data;
+
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+getUsers().then(data => {
+  console.log(data);
+
+  const preElement = document.getElementById('json-data');
+
+  preElement.style.fontSize = '18px';
+  
+  preElement.innerHTML = JSON.stringify(data);
+});
+
 
 //   const outputElement = document.getElementById('data');
 
